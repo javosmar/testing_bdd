29976c018161946235dbe4ced037cbc529ac395c
Martin Acosta <javosmar@gmail.com>
2020-12-07 12:41:00 -0300

    se completa el feature gestion

--- features/steps_definitions/lista_steps.js
+++ features/steps_definitions/lista_steps.js
@@ -5,14 +5,42 @@ const Lista = require('../../src/lista');
 
 let contexto = {};
 
+/**
+ * +++++++++++++++++++
+ * ++++++ GIVENs +++++++
+ * +++++++++++++++++++
+ */
 Given('una lista vacía', function () {
     contexto.lista = new Lista();
 });
 
+Given('una lista con los siguientes elementos', function (tabla) {
+    contexto.lista = new Lista();
+
+    tabla.rawTable.forEach((pareja) => {
+        contexto.lista.add(eval(pareja[0]), eval(pareja[1]));
+    });
+});
+
+/**
+ * +++++++++++++++++++
+ * ++++++ WHENs +++++++
+ * +++++++++++++++++++
+ */
 When('se agrega la pareja {}', function (pareja) {
+    pareja = JSON.parse(pareja);
     contexto.lista.add(Object.keys(pareja)[0], Object.values(pareja)[0]);
 });
 
-Then('la lista tiene {int} elemento almacenado', function (cantidad) {
-    expect(contexto.lista.count).to.equal(1);
+When('se elimina la clave {string}', function (clave) {
+    contexto.lista.delete(clave);
+});
+
+/**
+ * +++++++++++++++++++
+ * ++++++ THENs +++++++
+ * +++++++++++++++++++
+ */
+Then('la lista tiene {int} elemento(s) almacenado(s)', function (cantidad) {
+    expect(contexto.lista.count()).to.equal(cantidad);
 });
